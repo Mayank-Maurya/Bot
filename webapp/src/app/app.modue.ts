b2088436717550abc7e58b69305e52c9bce79e83
Mayank Maurya <mayank.maurya@proqio.com>
2024-02-02 02:04:23 +0530

    webApp initialized

---
+++ webapp/src/app/app.modue.ts
@@ -0,0 +1,13 @@
+// app.module.ts
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [], // Specify the initial component to load
+})
+export class AppModule {}
